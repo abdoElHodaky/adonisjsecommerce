7b85afa2a8cef622a11d24dece5ec599e26381b5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-12 22:03:47 +0200

    Update Database.js

--- database/seeds/Database.js
+++ database/seeds/Database.js
@@ -19,7 +19,7 @@ class DatabaseSeeder {
   * run () {
      var user=yield Factory.model('App/Model/User').create(1)
      var client=yield Factory.model('App/Model/Client').create(1)
-     var shop=yield Factory.model('App/Model/Shop').create(1)
+     var shop=yield Factory.model('App/Model/Shop').make(1)
      var cats=yield Factory.model("App/Model/Cat").create(3)
      cats.each(function *(cat){
      	var product=Factory.model("App/Model/Product").make(1)	
@@ -31,7 +31,7 @@ class DatabaseSeeder {
        //yield cat.products().save(product)
       // console.log(yield cat.products())
      })
-     console.log(user,shop)
+     console.log(user.attributes,shop)
      //shop.uid=user.id
      //yield shop.save()
   }
