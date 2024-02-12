380b9d07cdbb2472150dba0a69b4c47bfb43b918
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-12 22:21:02 +0200

    Update Database.js

--- database/seeds/Database.js
+++ database/seeds/Database.js
@@ -17,23 +17,23 @@ const Factory = use('Factory')
 class DatabaseSeeder {
 
   * run () {
-     var user=yield Factory.model('App/Model/User').create(1)
-     var client=yield Factory.model('App/Model/Client').create(1)
-     var shop=yield Factory.model('App/Model/Shop').create(1)
+     var user=Factory.model('App/Model/User').create(1)
+     yield Factory.model('App/Model/Client').create(1)
+     var shop=Factory.model('App/Model/Shop').create(1)
      var cats=yield Factory.model("App/Model/Cat").create(3)
      cats.each(function *(cat){
      	var product=Factory.model("App/Model/Product").make(1)	
       product.cid=cat.id
       yield product.save()
-      //cat.sid=shop.id
-     // yield cat.save()
+      cat.sid=shop.id
+      yield cat.save()
 
        //yield cat.products().save(product)
       // console.log(yield cat.products())
      })
-     console.log(user.attributes,shop.attributes)
-     //shop.uid=user.id
-     //yield shop.save()
+    // console.log(user.attributes,shop.attributes)
+     shop.uid=user.id
+     yield shop.save()
   }
   
 
