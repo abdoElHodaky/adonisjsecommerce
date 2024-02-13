8e85ac89f5dbaa6db060577008688f9c35ec4f47
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-02-13 10:39:23 +0200

    Update routes.js

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -17,7 +17,7 @@
 
 const Route = use('Route')
 Route.get("/",(request,respose)=>{
-   yield response.redirect("home")
+   return response.redirect("home")
 })
 Route.get("/Logout","UserController.logout")
 Route.resource("Home","UserController").addCollection("login", ['POST'])
